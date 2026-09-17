ae7c29633c4eaee37ff0af7cdb87b5dbc2779ec4
Abdelrahman Essam <eabdelrhman44@gmail.com>
2026-09-17 10:19:09 +0300

    Fix import case for OnBoardingModel

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/screens/to_do_screen.dart';
-import 'package:my_app/on_boarding_model.dart';
+import 'package:my_app/OnBoardingModel.dart';
 import '../sharedPref.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
